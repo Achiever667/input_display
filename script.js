d20b5ea81c5d2967fb294b356c04272ef2c36569
achiever667 <achiever667>
2020-06-23 12:18:02 +0100

    Displaying input values on the same webPage

---
+++ script.js
@@ -0,0 +1,15 @@
+   
+   
+   function showInput() {
+        document.getElementById('fNameDisplay').innerHTML = 
+                    document.getElementById("firstName").value;
+                    document.getElementById('lNameDisplay').innerHTML = 
+                    document.getElementById("lastName").value;
+                    document.getElementById('oNameDisplay').innerHTML = 
+                    document.getElementById("otherNames").value;
+                    document.getElementById('addressDisplay').innerHTML = 
+                    document.getElementById("address").value;
+                    document.getElementById('h1').innerHTML = 'Your Result';
+                   
+    }
+    
